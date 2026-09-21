f7564d175748752b42c54b3f40a8f09972765dea
akmalmaulan123-source <akmalmaulan123@gmail.com>
2026-09-21 14:42:13 +0700

    Update project

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal ('Dwi Akmal Maulana (282102719)');
+  protected readonly title = signal ('Dwi Akmal (282102719)');
 }
